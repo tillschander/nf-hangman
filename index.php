aa37103f328cdfd4e47f6cd493e86c094614da19
Till Schander <tillschander@gmail.com>
2021-03-16 15:15:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+$word = ['d', 'o', 'o', 'r'];
+$guesses = ['_', '_', '_', '_'];
+$turns = 5;
+$won = false;
+
+while ($turns > 0) {
+    echo join(' ', $guesses) . "\n";
+    echo "$turns turns left.\n";
+
+    $letter = trim(readline("Guess a letter: "));
+    $guessedCorrect = false;
+
+    for ($i=0; $i < count($word); $i++) {
+        if ($word[$i] === $letter) {
+            $guesses[$i] = $letter;
+            $guessedCorrect = true;
+        }
+    }
+
+    if ($guessedCorrect) {
+        echo "Yeah, correct letter!\n";
+    } else {
+        echo "Sorry, wrong letter!\n";
+        $turns--;
+    }
+}
+
+if ($won) {
+    echo "Yeah, you won!\n";
+} else {
+    echo "Sorry, you lost!\n";
+}
